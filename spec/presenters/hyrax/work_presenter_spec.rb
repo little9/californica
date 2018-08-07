f78841548f05514e20dd3a77739c352a24a8a739
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-08-07 11:36:55 -0500

    WIP - showing my work, seeking help

--- spec/presenters/hyrax/work_presenter_spec.rb
+++ spec/presenters/hyrax/work_presenter_spec.rb
@@ -3,8 +3,27 @@
 #  `rails generate hyrax:work Work`
 require 'rails_helper'
 
-RSpec.describe Hyrax::WorkPresenter do
-  it "has tests" do
-    skip "Add your tests here"
+RSpec.describe Hyrax::WorkPresenter, type: :presenter do
+  subject(:presenter) { described_class.new(document, ability, request) }
+  let(:ability)       { nil }
+  let(:document)      { SolrDocument.new(work.to_solr) }
+  let(:request)       { nil }
+  let(:work)          { Work.create(title: ['foo title']) }
+
+  describe '#export_as_ttl' do
+    let(:expected_fields) do
+      [:depositor, :title, :date_uploaded, :resource_type, :creator,
+       :contributor, :description, :keyword, :license, :rights_statement,
+       :publisher, :date_created, :subject, :language]
+    end
+
+    let(:properties) { work.class.properties }
+
+    it 'has expected predicates' do
+      predicates =
+        expected_fields.map { |f| properties[f.to_s].predicate.to_base }
+
+      expect(presenter.export_as_ttl).to include(*predicates)
+    end
   end
 end
